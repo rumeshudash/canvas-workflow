ed084cb806bfc25507f7860c2cf608b87b1ea829
Rumesh Udash <rumesh.udas@gmail.com>
2021-06-26 21:04:36 +0545

    Init

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,12 @@ import * as React from 'react'
 import styles from './styles.module.css'
 
 interface Props {
-  text: string
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const CanvasWorkflow = ({ }: Props) => {
+    return (
+        <div className={`canvas-workflow`}>
+
+        </div>
+    )
 }
