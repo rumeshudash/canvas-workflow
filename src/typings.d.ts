5317584f7c2bb7a22e207d1c512e03accbae7742
Rumesh Udash <rumesh.udas@gmail.com>
2021-06-30 21:54:45 +0545

    Add - Svg ts

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -11,3 +11,12 @@ declare module '*.png'{
     const src: string;
     export default src;
 }
+
+interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
+
+declare module '*.svg' {
+  const svgUrl: string;
+  const svgComponent: SvgrComponent;
+  export default svgUrl;
+  export { svgComponent as ReactComponent }
+}
